2ffff2cba2f7513f54387efbbd4c0a8ab02126c0
Eric M <emo88.em@gmail.com>
2019-05-30 22:28:05 -0500

    added switch to make 404 page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 const ExpenseDashboardPage = () => (
     <div>
@@ -13,28 +13,36 @@ const AddExpensePage = () => (
     <div>
         This is from my add expense page!
     </div>
-)
+);
 
 const EditPage = () => (
     <div>
         This is the edit page!
     </div>
-)
+);
 
 const HelpPage = () => (
     <div>
-        This is the help page!
+        This is the help page edit page!
+    </div>
+);
+
+const notfoundpage = () => (
+    <div>
+        404! Page not found! 
     </div>
 )
 
+
 const routes = (
     <BrowserRouter> 
-        <div>
+        <Switch>
         <Route path="/" component={ExpenseDashboardPage}  exact={true}/>
-        <Route path="/create" component={AddExpensePage} exact={true}/> 
-        <Route path="/edit" component={EditPage} exact={true}/> 
-        <Route path="/help" component={HelpPage} exact={true}/> 
-        </div>
+        <Route path="/create" component={AddExpensePage} /> 
+        <Route path="/edit" component={EditPage} /> 
+        <Route path="/help" component={HelpPage} /> 
+        <Route component={notfoundpage} />
+        </Switch>
     </BrowserRouter>
 
 );
