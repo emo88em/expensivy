5104025c63d77315b38b486da02858b59071f7a6
Eric M <emo88.em@gmail.com>
2019-06-02 13:34:36 -0500

    add routers folder and added route pages

--- src/index.js
+++ src/index.js
@@ -1,50 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
-
-const ExpenseDashboardPage = () => (
-    <div>
-        This is from my dashboard component!
-    </div>
-);
-
-const AddExpensePage = () => (
-    <div>
-        This is from my add expense page!
-    </div>
-);
-
-const EditPage = () => (
-    <div>
-        This is the edit page!
-    </div>
-);
-
-const HelpPage = () => (
-    <div>
-        This is the help page edit page!
-    </div>
-);
-
-const notfoundpage = () => (
-    <div>
-        404! Page not found! <Link to="/">Go Home</Link>
-    </div>
-)
+import AppRouter from './routers/AppRouter';
 
 
-const routes = (
-    <BrowserRouter> 
-        <Switch>
-        <Route path="/" component={ExpenseDashboardPage}  exact={true}/>
-        <Route path="/create" component={AddExpensePage} /> 
-        <Route path="/edit" component={EditPage} /> 
-        <Route path="/help" component={HelpPage} /> 
-        <Route component={notfoundpage} />
-        </Switch>
-    </BrowserRouter>
-
-);
-
-ReactDOM.render(routes, document.getElementById('root'));
+ReactDOM.render(<AppRouter />, document.getElementById('root'));
---
+++ src/routers/EditPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+;
+const EditPage = () => (
+    <div>
+        This is the edit page!
+    </div>
+);
+
+export default EditPage;
